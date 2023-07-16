d4b276ad0c5ca21ccc0e54c1a1f5e574e16533f0
Sayyed Mohammad Miri <smmmiri@outlook.com>
2023-07-16 12:03:38 +0330

    Added Shape class

---
+++ ShapesAreaCalculatorApplication/Shape.cs
@@ -0,0 +1,18 @@
+﻿namespace ShapesAreaCalculatorApplication
+{
+    internal class Shape
+    {
+        protected double Width { get; set; }
+        protected double Height { get; set; }
+        protected readonly double Area;
+
+        public Shape()
+        {
+            Area = Width * Height;
+        }
+
+        protected virtual void DisplayInformations()
+        {
+        }
+    }
+}
